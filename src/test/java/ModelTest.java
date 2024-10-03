0d8e1e5115db596ea5e3f0bfe40557fcf70eb799
mmatic64 <mmatic@student.tgm.ac.at>
2024-10-03 09:20:12 +0200

    Test v1.0 verbessert

--- src/test/java/ModelTest.java
+++ src/test/java/ModelTest.java
@@ -2,6 +2,7 @@ import mmatic.model.*;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,13 +16,18 @@ public class ModelTest {
         Paar paar = new Paar("Wort", "https://www.google.com");
         assertEquals("Wort", paar.getWort());
         assertEquals("https://www.google.com", paar.getURL());
-        assertThrows(new Paar("wort", "htt://www.google.com"))
+        assertThrows(IllegalArgumentException.class, () -> new Paar("wort", "htt://www.google.com"));
+        assertThrows(IllegalArgumentException.class, () -> new Paar("wort", "http://www..google.com"));
+        assertThrows(IllegalArgumentException.class, () -> new Paar("wort", "http://www.google.com."));
+        assertThrows(IllegalArgumentException.class, () -> new Paar("wort", "http://google"));
+        assertThrows(IllegalArgumentException.class, () -> new Paar("w.", "http://www.google.com"));
     }
 
     @Test
     void setTrainer() {
         Paar paar = new Paar("Wort", "https://www.google.com");
-        Trainer trainer = new Trainer(paar);
+        Trainer trainer = new Trainer();
+        trainer.addPaar(paar);
         trainer.select();
         assertEquals(paar, trainer.getBild());
     }
@@ -31,7 +37,8 @@ public class ModelTest {
         Paar paar1 = new Paar("Wort1", "https://www.google1.com");
         Paar paar2 = new Paar("Wort2", "https://www.google2.com");
         Paar paar3 = new Paar("Wort3", "https://www.google3.com");
-        Trainer trainer = new Trainer(paar1);
+        Trainer trainer = new Trainer();
+        trainer.addPaar(paar1);
         trainer.addPaar(paar2);
         trainer.addPaar(paar3);
         trainer.select(1);
